2b4152e6d5946d1af0fcb85741e16d206770e7a4
賴昱劭 <laiyushao@laiyushaodeMacBook-Pro.local>
2021-07-10 13:18:09 +0800

    20210710 Ted fix

---
+++ JS/Result.js
@@ -0,0 +1,125 @@
+$(document).ready(function() {
+    //$(".coverB img").click(function()
+    $(".A").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".A a").attr("href", "ResultSun.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Sun.png");
+    });
+    $(".B").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B a").attr("href", "ResultOyster.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Oyster.png");
+    });
+    $(".C").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C a").attr("href", "ResultSea.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Sea.png");
+    });
+    $(".D").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C a").attr("href", "ResultHeat.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Heat.png");
+    });
+    $(".E").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C a").attr("href", "ResultBaby.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Baby.jpeg");
+    });
+    $(".F").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C a").attr("href", "ResultGuide.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Guide.png");
+    });
+    $(".G").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C a").attr("href", "ResultView.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/View.png");
+    });
+    $(".H").mouseover(function()
+    {
+        //var x = document.getElementById("demo");
+        //alert("Hi");
+        $(".A img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".B img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".C img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".D img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".E img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".F img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".G img").attr("src", "../IMAGE/Result/入口/null.png");
+        $(".H img").attr("src", "../IMAGE/Result/入口/mark.png");
+        $(".C a").attr("href", "ResultResearch.html");
+        $(".Frame").attr("src", "../IMAGE/Result/入口/Research.png");
+    });
+});
